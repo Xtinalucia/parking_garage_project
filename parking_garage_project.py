decf891275323ec09208ec8b838041622cdf61d4
jdecee <jdecee@gmail.com>
2021-09-24 19:45:26 -0400

    major update

--- parking_garage_project.py
+++ parking_garage_project.py
@@ -1,95 +1,91 @@
+from typing import Counter
+
+
 class Garage():
-#garage has 10 tickets/parking spaces
+    NextTicketNumber =0
+
     def __init__(self):
-        self.tickets =[]
+        self.tickets = []
         self.parkingSpaces = []
         self.currentTicket = {}
-        
+        self.counter = 0
+        self.spots = 100
+ 
     def takeTicket(self):
-      #remove ticket /parking here  
-        ticketnumber = 0
-        totaltickets = self.tickets.append(ticketnumber + 1)
-        print(totaltickets)    
+        # Create ticket object
+        thisTicket = Ticket(self.NextTicketNumber)
+        #Increment counter
+        self.NextTicketNumber += 1
+        #Decrement spots
+        self.spots -= 1
+        #Add ticekt to list
+        self.tickets.append(thisTicket)
+        print(f'Your ticket number is {thisTicket.ticketnum}')
         
-        #self.parking -=1
-        #print(f'There are {self.parking} parking spaces left')
-        #self.currentTicket["ticket 1"] = "False"
         
     def payForParking(self):
-         amount = int(input("Pay amount here! "))
-         if amount !=0:
-            print('Your ticket has been paid, and you have 15 minutes to leave!')
-         else:
-            print('Don\'t be a cheapskate, pay your ticket bro!')
-    #Update the "currentticket" dict key "paid" to True
+         ticketId = int(input('What is your ticket number? '))
+         ticketFound = False
+         for i in self.tickets:
+             if i.ticketnum == ticketId:
+                 ticketFound = True
+                 amount = int(input("Pay amount here! "))
+                 if amount !=0:
+                    i.paid = True
+                    print('Your ticket has been paid, and you have 15 minutes to leave!')
+                 else:
+                    print('Don\'t be a cheapskate, pay your ticket bro!')
+         if not ticketFound:
+             print('The ticket number was not found')
     
-    #def leaveGarage(self):  
-        #Validate ticket - paid/unpaid
-        #print('Ticket number please?')
-        #if self.ticket = # valid ticket# thats paid:
-        #print('Thank you, have a nice day!')
-        #if self.ticket = #upaid:
-        #input("Provide a valid ticket or Pay.")
-        #Loop back to prev print statement
+    def leaveGarage(self): 
+         ticketId = int(input('What is your ticket number? '))
+         ticketFound = False
+         for i in self.tickets:
+             if i.ticketnum == ticketId:
+                  self.spots += 1
+                  ticketFound = True
+                  if i.paid == True:
+                    print('Have a nice day')
+                  else:
+                    print('This ticket is unpaid, please pay at ticket machine')
 
-        #self.ticket +=1
-        #self.parking +=1
-    
-#class Tickets():
-
-   # def __init__(self, ticketnum):
-        #self.ticketnum = ticketnum
         
-#class PkSpots():
+class Ticket():
+
+   def __init__(self, ticketnum):
+        self.ticketnum = ticketnum
+        self.paid = False
+
+
+
+def run():
+    GarageObj = Garage()
+    quit = False
+    while not quit:
+        print('Hello Customer!')
+        print('')
+        print('1 - Get ticket')
+        print('2 - Pay for ticket')
+        print('3 - Leave garage')
+        print('4 - Quit')
+        print('')
+        option  = int(input('Please select'))
+        if option == 1:
+            GarageObj.takeTicket()
+        elif option ==2:
+           GarageObj.payForParking()
+        elif option ==3:
+            GarageObj.leaveGarage()
+        elif option ==4:
+            quit = True
+
+run()
+
         
-    #def __init__(self, avalspots):
-        #self.avalspots = avalspots
 
-
-
- 
-
-test = Garage()
-test.takeTicket()
-#test.payForParking()
-                
 
 
     
-#         pass   
 
-# def add(self):
-#         item_name = input('What is the name of the item you would like to add? ')
-#         item_price = float(input(f'What is the price of {item_name}? '))
-#         item = Item(item_name, item_price)
-#         self.cart.append(item)
-#         print(f'{item.name} has been added to your cart.')
-    
-#     def remove(self):
-#         item_to_remove = input("What item would you like to remove? ")
-#         cart_item_names = set([item.name for item in self.cart])
-#         if item_to_remove in cart_item_names:
-#             for i in range(len(self.cart)):
-#                 if self.cart[i].name == item_to_remove:
-#                     removed_item = self.cart.pop(i)
-#                     break
-#             print(f"{removed_item.name} has been removed from your cart.")
-#         else:
-#             print(f"{item_to_remove} is not in your cart")
-    
-#     def clear(self):
-#         self.cart.clear()
-#         print("Your cart has been cleared")
-    
-#     def show(self):
-#         for item in self.cart:
-#             print(f"{item.name} - ${item.price}")
-            
-#     def get_total(self):
-#         total = 0
-#         for item in self.cart:
-#             total += item.price
-#         return total
-    
-
-# my_company = run()
+        
